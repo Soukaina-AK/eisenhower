5e51f371e6acf790b009d9c6667a9e914e506e5d
Soukaina-AK <soukainaakaroum05@gmail.com>
2025-10-23 13:03:00 +0100

    Fix SideBar Responsive

--- src/components/task.tsx
+++ src/components/task.tsx
@@ -2,7 +2,7 @@ import { Edit } from "lucide-react";
 
 export default function Task() {
   return (
-    <div className="flex flex-col items-start justify-center gap-3 bg-white rounded-xl overflow-hidden p-2 pb-3">
+    <div className="flex flex-col items-start justify-center gap-3 bg-white rounded-xl overflow-hidden p-2 pb-3 min-h-[55px]">
         <div className="flex flex-row items-center gap-1">
             <input type="radio" name="completed" id="completed" />
             <div>Lorem ipsum dolor sit amet  </div>
--- src/sections/side-bar.tsx
+++ src/sections/side-bar.tsx
@@ -16,16 +16,21 @@ export default function SideBar() {
     }
 
   return (
-    <section className="min-w-[300px] h-[100%] task-list rounded-xl rounded-bl-none lg:rounded-bl-xl overflow-hidden border-[0.9px] border-dotted border-sky-700 bg-csky relative">
+    <section className="min-w-[300px] h-[100%]  rounded-xl rounded-bl-none lg:rounded-bl-xl overflow-hidden border-[0.9px] border-dotted border-sky-700 bg-csky">
         <div className="bg-csky p-3">ToDo</div>
-        <section className="p-2 flex flex-col gap-2 ">
-
-            {Array.from({ length: 10 }).map((_, i) => (
+        <section className="p-2 flex flex-col h-full">
+            <div className="flex-[0.9] flex flex-col gap-2 overflow-auto task-list pb-3">
+                {Array.from({ length: 20 }).map((_, i) => (
                 <Task key={i} />
-            ))}
+                ))}
+            </div>
 
-            <div className="sticky bottom-0">
-                {openAddTask === true ? <Input onClose={handleCloseTaskForm} /> : <AddTaskButton onClick={handleOpenTaskForm}/>}
+            <div className="sticky bottom-1 bg-csky ">
+                {openAddTask ? (
+                <Input onClose={handleCloseTaskForm} />
+                ) : (
+                <AddTaskButton onClick={handleOpenTaskForm} />
+                )}
             </div>
         </section>
     </section>
